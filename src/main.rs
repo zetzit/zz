9300cb7e69d84324493de8bc82251dab6947bd95
Arvid E. Picciani <aep@exys.org>
2019-06-20 23:12:11 +0200

    m

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use std::io::Write;
 use std::path::Path;
 use std::collections::HashMap;
 use std::fs;
+use std::process::Command;
 
 #[derive(Parser)]
 #[grammar = "zz.pest"]
@@ -16,12 +17,19 @@ pub struct Emitter{
     f: fs::File
 }
 
+pub enum Visibility {
+    Shared,
+    Object,
+    Export,
+}
 
 #[derive(Default)]
 pub struct Module {
     name:       String,
     functions:  HashMap<String, Function>,
-    imports:    Vec<String>
+    imports:    Vec<String>,
+    structs:    Vec<Struct>,
+    includes:   Vec<String>,
 }
 
 pub struct AnonArg {
@@ -40,6 +48,14 @@ pub struct Function {
     ret:    Option<AnonArg>,
     args:   Vec<NamedArg>,
     name:   String,
+    body:   String,
+    vis:    Visibility,
+}
+
+pub struct Struct {
+    name:   String,
+    body:   String,
+    vis:    Visibility,
 }
 
 impl Emitter {
@@ -50,14 +66,33 @@ impl Emitter {
         Emitter{f}
     }
 
-    pub fn define(&mut self, md: &str, f: &Function) {
-        write!(self.f, "{} {}_{} (",
-                 match &f.ret {
-                     None       => "void",
-                     Some(a)    => &a.typ,
-                 },
-                 md,
-                 f.name).unwrap();
+    pub fn struc(&mut self, ns: Option<&str>, s: &Struct) {
+        write!(self.f, "typedef struct \n{}\n",
+                 s.body,
+                 ).unwrap();
+
+        if let Some(ns) = ns {
+            write!(self.f, "{}_",ns).unwrap();
+        }
+
+        write!(self.f, "{} ;\n", s.name).unwrap();
+    }
+
+    pub fn function(&mut self, ns: Option<&str>, f: &Function, body: Option<&str>) {
+        if let Visibility::Object = f.vis {
+            write!(self.f, "static ").unwrap();
+        }
+
+        match &f.ret {
+            None       => write!(self.f, "void "),
+            Some(a)    => write!(self.f, "{} ", &a.typ),
+        }.unwrap();
+
+        if let Some(ns) = ns {
+            write!(self.f, "{}_",ns).unwrap();
+        }
+
+        write!(self.f, "{} (", f.name).unwrap();
 
         let mut first = true ;
         for arg in &f.args {
@@ -83,9 +118,20 @@ impl Emitter {
 
             write!(self.f, " {}", arg.name).unwrap();
         }
-        write!(self.f, ");\n").unwrap();
-
+        match body {
+            None => write!(self.f, ");\n").unwrap(),
+            Some(b) => write!(self.f, ") \n{}\n\n", b).unwrap(),
+        }
     }
+
+    pub fn include(&mut self, i: &str) {
+        let guardname : String = i.chars().filter(|&c| "abcdefghijklmnopqrstuvwxyz1234567890".contains(c)).collect();
+        write!(self.f, "#ifndef ZZ_INCLUDE_{}\n", guardname).unwrap();
+        write!(self.f, "#define ZZ_INCLUDE_{}\n", guardname).unwrap();
+        write!(self.f, "#include {}\n", i).unwrap();
+        write!(self.f, "#endif\n").unwrap();
+    }
+
 }
 
 
@@ -97,21 +143,66 @@ fn main() {
 
     for (name, md) in &modules {
         let mut em = Emitter::new(&(name.clone() + ".c"));
-        for (_,fun) in &md.functions {
-            em.define(&name, &fun);
+        for i in &md.includes {
+            em.include(i);
         }
         for mp in &md.imports {
             match modules.get(mp) {
                 None => panic!("{}: imports unknown module {}", name, mp),
                 Some(m2) => {
+                    for i in &m2.includes {
+                        em.include(i);
+                    }
                     for (_,fun) in &m2.functions {
-                        em.define(&m2.name, &fun);
+                        if let Visibility::Object  = fun.vis { continue };
+                        em.function(None, &fun, None);
+                        //em.function(Some(&m2.name), &fun, None);
+                    }
+                    for s in &m2.structs {
+                        if let Visibility::Object  = s.vis { continue };
+                        em.struc(None, &s);
+                        //em.struc(Some(&m2.name), &s);
                     }
                 }
             }
-
+        }
+        for s in &md.structs {
+            em.struc(None, &s);
+        }
+        for (_,fun) in &md.functions {
+            em.function(None, &fun, Some(&fun.body));
         }
     }
+
+
+    let mut linkargs  = Vec::new();
+    for (name, md) in &modules {
+        let inp  = format!("./target/c/{}.c", name);
+        let outp = format!("./target/c/{}.o", name);
+
+        let status = Command::new("clang")
+            .args(&["-c", &inp, "-o", &outp])
+            .status()
+            .expect("failed to execute cc");
+
+        if !status.success() {
+            std::process::exit(status.code().unwrap_or(3));
+        }
+
+        linkargs.push(outp);
+    }
+
+    linkargs.push("-o".into());
+    linkargs.push("./target/exe".into());
+
+    let status = Command::new("clang")
+        .args(&linkargs)
+        .status()
+        .expect("failed to execute linker");
+    if !status.success() {
+        std::process::exit(status.code().unwrap_or(3));
+    }
+
 }
 
 fn pp(modules: &mut HashMap<String, Module>, n: &Path)
@@ -140,12 +231,23 @@ fn p(modules: &mut HashMap<String, Module>, n: &Path) -> Result<Module, pest::er
         match decl.as_rule() {
             Rule::function => {
                 let mut decl = decl.into_inner();
-                let name : String = decl.next().unwrap().as_str().into();
+                let mut name = String::new();
                 let mut args = Vec::new();
                 let mut ret  = None;
+                let mut body = String::new();
+                let mut vis = Visibility::Shared;
 
                 for part in decl {
                     match part.as_rule() {
+                        Rule::key_private => {
+                            vis = Visibility::Object;
+                        }
+                        Rule::key_pub => {
+                            vis = Visibility::Export;
+                        }
+                        Rule::ident => {
+                            name = part.as_str().into();
+                        }
                         Rule::ret_arg => {
                             ret = Some(AnonArg{
                                 typ: part.into_inner().as_str().to_string()
@@ -153,13 +255,13 @@ fn p(modules: &mut HashMap<String, Module>, n: &Path) -> Result<Module, pest::er
                         },
                         Rule::fn_args => {
                             for arg in part.into_inner() {
-                                let mut arg = arg.into_inner();
-                                let types  = arg.next().unwrap();
-                                let name    = arg.next().unwrap().as_str().to_string();
-                                let mut muta        = false;
-                                let mut ptr         = false;
-                                let mut typ         = String::new();
-                                let mut namespace   = None;
+                                let mut arg       = arg.into_inner();
+                                let types         = arg.next().unwrap();
+                                let name          = arg.next().unwrap().as_str().to_string();
+                                let mut muta      = false;
+                                let mut ptr       = false;
+                                let mut typ       = String::new();
+                                let mut namespace = None;
 
                                 for part in types.into_inner() {
                                     match part.as_rule() {
@@ -191,7 +293,8 @@ fn p(modules: &mut HashMap<String, Module>, n: &Path) -> Result<Module, pest::er
                                 });
                             }
                         },
-                        Rule::block=> {
+                        Rule::block => {
+                            body = part.as_str().to_string();
                         },
                         e => panic!("unexpected rule {:?} in function", e),
                     }
@@ -201,15 +304,53 @@ fn p(modules: &mut HashMap<String, Module>, n: &Path) -> Result<Module, pest::er
                     name,
                     ret,
                     args,
+                    body,
+                    vis,
                 });
             },
             Rule::EOI=> {},
+            Rule::struct_d => {
+                let mut decl = decl.into_inner();
+
+                let mut vis   = Visibility::Shared;
+                let mut name  = None;
+                let mut body  = None;
+
+                for part in decl {
+                    match part.as_rule() {
+                        Rule::key_private => {
+                            vis = Visibility::Object;
+                        }
+                        Rule::key_pub => {
+                            vis = Visibility::Export;
+                        }
+                        Rule::ident => {
+                            name = Some(part.as_str().into());
+                        }
+                        Rule::struct_c => {
+                            body = Some(part.as_str().into());
+                        }
+                        e => panic!("unexpected rule {:?} in struct ", e),
+                    }
+                };
+
+
+                module.structs.push(Struct {
+                    name: name.unwrap(),
+                    body: body.unwrap(),
+                    vis,
+                });
+            }
             Rule::import => {
                 let im = decl.into_inner().as_str();
                 if !modules.contains_key(im) {
                     pp(modules, &Path::new(im).with_extension("zz"));
-                    module.imports.push(im.to_string());
                 }
+                module.imports.push(im.to_string());
+            },
+            Rule::include => {
+                let im = decl.into_inner().as_str();
+                module.includes.push(im.to_string());
             },
             e => panic!("unexpected rule {:?} in file", e),
 
