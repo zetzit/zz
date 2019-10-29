79ceb6d4b73dcee14936f46623346995c4cf2cd0
Arvid E. Picciani <aep@exys.org>
2019-10-29 16:54:02 +0100

    fix byteorder mod

--- modules/byteorder/src/lib.h
+++ modules/byteorder/src/lib.h
@@ -27,3 +27,18 @@
         #error cannot find a definition of BIG_ENDIAN
     #endif
 #endif
+
+#if BYTE_ORDER == LITTLE_ENDIAN
+    #if BYTE_ORDER == BIG_ENDIAN
+        #error "compiler says byte order is both little and big endian"
+    #endif
+#else
+    #if BYTE_ORDER == BIG_ENDIAN
+        
+    #else
+        #error "compiler says byte order is neither little nor big endian"
+    #endif
+#endif
+
+#if BYTE_ORDER == LITTLE_ENDIAN && BYTE_ORDER == BIG_ENDIAN
+#endif
