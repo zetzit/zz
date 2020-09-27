4d56f2b35da3ecc9a5d2d7cb58f7fd843b4097bb
Arvid E. Picciani <aep@exys.org>
2020-09-27 12:07:34 +0200

    minor compiler fixes

--- modules/map/src/siphash.h
+++ modules/map/src/siphash.h
@@ -76,7 +76,7 @@
 #define TRACE
 #endif
 
-int siphash(const uint8_t *in, const size_t inlen, const uint8_t *k,
+static inline int siphash(const uint8_t *in, const size_t inlen, const uint8_t *k,
             uint8_t *out, const size_t outlen) {
 
     assert((outlen == 8) || (outlen == 16));
